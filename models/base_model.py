b4a29a09f0f385f2527682ddc1f12c14ec0ad912
drwamunga <87418352+drwamunga@users.noreply.github.com>
2022-01-28 23:56:08 +0300

    Class base model

--- models/base_model.py
+++ models/base_model.py
@@ -3,3 +3,12 @@
 """Defines a base class BaseModel"""
 import uuid
 import datetime
+
+
+class BaseModel:
+    """Defines all common attributes for other classes"""
+    def __init__(self, *args, **kwargs):
+        """Runs on instantiation of BaseModel class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
